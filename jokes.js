da567ce2e0a413cefd6af4352c0f931148950781
maryamawit samson <maryamawitsamson43@gmail.com>
2026-04-30 13:45:44 +0300

    Jokes made by HTML,JS

---
+++ jokes.js
@@ -0,0 +1,19 @@
+document.getElementById("btn").addEventListener("click",function(e){
+   whole();
+})
+ const main=document.getElementById("main");
+        main.innerHTML=JSON.parse(localStorage.getItem("key"));
+async function whole(){
+     try{ 
+       
+        
+        const jokes= await fetch('https://api.chucknorris.io/jokes/random');
+        const json= await jokes.json()
+        localStorage.setItem("key",JSON.stringify(json.value))
+       main.innerHTML=json.value;
+      
+     }
+catch(error){
+    console.log(error)
+}
+}
